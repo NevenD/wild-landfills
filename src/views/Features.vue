df737aded2a171422006e99d2db626e54ef2e16a
Neven Dimač <neven.dimac@gmail.com>
2019-09-14 18:54:04 +0200

    Adding description features.

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -10,29 +10,25 @@
         <span>Sort functionalities by name</span>
       </v-tooltip>
 
-      <!-- <v-tooltip top>
-          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
-            <v-icon left small>person</v-icon>
-            <span class="caption text-lowercase">By person name</span>
-          </v-btn>
-          <span>Sort projects by person name</span>
-      </v-tooltip>-->
+      <v-tooltip top>
+        <v-btn small flat color="grey" @click="sortBy('desc')" slot="activator">
+          <v-icon left small>tab</v-icon>
+          <span class="caption text-lowercase">By description</span>
+        </v-btn>
+        <span>Sort implementation by description</span>
+      </v-tooltip>
     </v-layout>
     <v-card v-for="project in projects" :key="project.title">
       <v-layout row wrap :class="`pa-3 project ${project.status}`">
-        <v-flex xs12 md3>
+        <v-flex xs12 md4>
           <div class="caption grey--text">App functionalities</div>
           <div>{{project.title}}</div>
         </v-flex>
-        <v-flex xs12 sm4 md3>
-          <div class="caption grey--text">Tool</div>
-          <div v-html="project.tools"></div>
-        </v-flex>
-        <v-flex xs12 sm4 md3>
+        <v-flex xs12 sm4 md4>
           <div class="caption grey--text">Details</div>
           <div>{{project.desc}}</div>
         </v-flex>
-        <v-flex xs12 sm4 md3>
+        <v-flex xs12 sm4 md4>
           <div class="right">
             <v-chip small :class="`${project.status} white--text caption my-2`">{{project.status}}</v-chip>
           </div>
@@ -49,92 +45,75 @@ export default {
     return {
       projects: [
         {
-          title: "Implemented WMS Layers",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_source_TileWMS.html' target='_blank' rel='noopener noreferrer'>TileWMS</a> ",
+          title: "Added map view with vector list",
           desc:
-            "Added 5 raster layers (digital ortofoto 2011/2016, topographic map, corine land cover and bing map )",
+            "Created map view with popup window that contains list of drawn features",
           status: "implemented"
         },
         {
-          title: "Added vector layers",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_format_GeoJSON-GeoJSON.html' target='_blank' rel='noopener noreferrer'>Geojson</a> ",
+          title: "Integrated vector layer",
           desc:
-            "Implemented read, delete, zoom to extent, get feature properties on click",
+            "Implemented view, zoom to extent and get feature properties on click",
           status: "implemented"
         },
         {
-          title: "Added interactions",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_interaction_Interaction-Interaction.html' target='_blank' rel='noopener noreferrer'>Interactions</a> ",
+          title: "Added draw polygon interaction",
           desc:
-            "Implemented modify and snapping for vector layers. Added rotation and reset rotation with animated zoom and reset view. Added opacity for all layers from layer dialog",
+            "Integrated ability to draw wild landfills with properties such as: name, description, location, recorded, date and automatic calculation of area",
           status: "implemented"
         },
         {
-          title: "Added draw geometry",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_interaction_Draw-Draw.html' target='_blank' rel='noopener noreferrer'>Draw geometry</a> ",
+          title: "Added view interactions",
           desc:
-            "Added drawing geometry type for point,line,polygon and circle.",
+            "Changed visuals of zoom in, zoom out, zoom to extent and hove view. Added ability to reset rotation if rotation is not 0",
           status: "implemented"
         },
         {
           title: "Added map controls",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_control.html' target='_blank' rel='noopener noreferrer'>Map controls</a> ",
           desc:
             "Added ability to show/hide additional map elements like attribution, coordinates, scalebar and minimap",
           status: "ongoing"
         },
         {
           title: "Added measure feature",
-          tools:
-            "<a href='https://openlayers.org/en/latest/examples/measure.html' target='_blank' rel='noopener noreferrer'>Measure example</a> ",
           desc: "Implemented ability to measure area and length",
           status: "implemented"
         },
         {
           title: "Implemented vector drag and drop",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_interaction_DragAndDrop-DragAndDrop.html' target='_blank' rel='noopener noreferrer'>Drag/drop interaction</a> ",
           desc:
             "Added ability to drag and drop GPX,GeoJSON,IGC,KML,TopoJSON and render vectors. Implemented modify option for dragged vectors (only for geojson). Ability to download modified vectors",
           status: "implemented"
         },
         {
           title: "Parse vector shapefiles",
-          tools: "N/A",
+
           desc: "Implement functionality to upload and edit shapefile vectors",
           status: "todo"
         },
         {
           title: "Consume spatial data via API",
-          tools: "N/A",
+
           desc:
             "Using AXIOS to consume spatial data and render them on map with ability to read properties",
           status: "todo"
         },
         {
           title: "Implemented WFS functionality",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_format_WFS-WFS.html' target='_blank' rel='noopener noreferrer'>WFS Vectors</a> ",
+
           desc:
             "Implement feature format for reading and writing data in the WFS format",
           status: "todo"
         },
         {
           title: "Add HeatMaps",
-          tools:
-            "<a href='https://openlayers.org/en/latest/apidoc/module-ol_layer_Heatmap-Heatmap.html' target='_blank' rel='noopener noreferrer'>Heatmaps</a> ",
+
           desc: "Implement layers for rendering vector data as a heatmap",
           status: "todo"
         },
         {
           title: "Integrate existing frontend functionality with .NET Core",
-          tools:
-            "<a href='https://docs.microsoft.com/en-us/dotnet/core/about' target='_blank' rel='noopener noreferrer'>.NET Core</a> ",
+
           desc:
             "Create backend logic for existing app. Create functionality for adding properties and vector geometries i db (SQL or PostgreSQL).Save and render geometry using .NET Core ",
           status: "todo"
